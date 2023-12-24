a239b533df57328d945086f7fba84e6e0ffbd2b3
Bonnie Simon <bonnie.developer@gmail.com>
2023-12-24 16:42:47 +0530

    Implement random walk using multiple points

--- src/main.c
+++ src/main.c
@@ -1,29 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "raylib.h"
 #include "raymath.h"
-#include "stdio.h"
 
 #define SCREEN_WIDTH 1600
 #define SCREEN_HEIGHT 900
+#define NUMBER_OF_POINTS 10000
 
-void find_next_point_to_draw(int *x, int *y) {
+typedef struct {
+  int x;
+  int y;
+} Point;
+
+void draw(int point_index, Point *points) {
+  Point previous_point = *(points + (point_index - 1));
+  Point *current_point = (points + point_index);
+
   int random_value = GetRandomValue(0, 3);
-  printf("random_value: %d\n", random_value);
 
-  int jump_value = 3;
+  int jump_value = 6;
 
   switch (random_value) {
     case 0:
-      *y = *y - jump_value;
+      current_point->y = previous_point.y - jump_value;
+      current_point->x = previous_point.x;
       break;
     case 1:
-      *x = *x + jump_value;
+      current_point->x = previous_point.x + jump_value;
+      current_point->y = previous_point.y;
       break;
     case 2:
-      *y = *y + jump_value;
+      current_point->y = previous_point.y + jump_value;
+      current_point->x = previous_point.x;
       break;
     case 3:
-      *x = *x - jump_value;
+      current_point->x = previous_point.x - jump_value;
+      current_point->y = previous_point.y;
       break;
+  }
+
+  for (int i = 0; i < point_index + 1; i++) {
+    Point point_to_be_drawn = *(points + i);
+    DrawCircle(point_to_be_drawn.x, point_to_be_drawn.y, 1, GOLD);
   }
 }
 
@@ -33,13 +52,22 @@ int main() {
   int x = SCREEN_WIDTH / 2;
   int y = SCREEN_HEIGHT / 2;
 
-  ClearBackground(DARKGRAY);
+  Point *points = (Point *)malloc(NUMBER_OF_POINTS * sizeof(Point));
+
+  Point init_point = {.x = x, .y = y};
+  *points = init_point;
+
+  int point_index = 1;
   while (!WindowShouldClose()) {
     BeginDrawing();
-    DrawCircle(x, y, 3, GOLD);
+    {
+      ClearBackground(DARKGRAY);
 
-    find_next_point_to_draw(&x, &y);
-
+      if (point_index < NUMBER_OF_POINTS) {
+        draw(point_index, points);
+        point_index++;
+      }
+    }
     EndDrawing();
   }
 
